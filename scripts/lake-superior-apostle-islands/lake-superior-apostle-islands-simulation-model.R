0b384d32447d50ac3125c0eb12a606b57d1b1b64
Taylor Stewart <taylor.stewart@uvm.edu>
2021-06-24 20:11:11 -0400

    Update lake-superior-apostle-islands-simulation-model.R

--- scripts/lake-superior-apostle-islands/lake-superior-apostle-islands-simulation-model.R
+++ scripts/lake-superior-apostle-islands/lake-superior-apostle-islands-simulation-model.R
@@ -24,7 +24,8 @@ simulation.files <- list.files('data/climate-simulations/simstrat-summaries/byCl
 
 simulation.data <- do.call(rbind, lapply(simulation.files, function(i) {
   data <- fread(i)
-}))
+})) %>% 
+  filter(year.class >= 2010)
 
 model.locations <- read_excel("data/model-population-parameters.xlsx", sheet = "bio-parameters") %>% 
   filter(population == "apostle islands")
@@ -35,19 +36,71 @@ simulation.data.filt <- simulation.data %>%
 
 
 ## Stewart et al. 2021
-model.superior <- read_excel("data/model-structural-parameters.xlsx", sheet = "coefficients") %>% 
+model.parameters <- read_excel("data/model-structural-parameters.xlsx", sheet = "coefficients") %>% 
   filter(lake == "Lake Superior")
 
 
-lapply(unique(simulation.data.filt$year.class), function(i) {
-  simulation.data.annual <- simulation.data.filt %>% filter(year == i)
+simulation.model.hatch <- do.call(rbind, lapply(unique(simulation.data.filt$year.class), function(i) {
+  ## filter to a single winter
+  simulation.data.annual <- simulation.data.filt %>% filter(year.class == i)
   
-  model.locations$start.spawn.temp_c
+  ## Calculate a 5-day center moving average to smooth temperature curve
+    ## Smoothing prevents issues below trying to find the start and stop from large daily temp deviations
+  simulation.data.annual.ma <- simulation.data.annual %>% group_by(scenario) %>% 
+    mutate(temp.ma_c = frollmean(mean.temp.c, n = 5, align = "center"))
   
-  model.locations$end.spawn.temp_c
+  ## Calculate the start date of spawning period
+  spawn.start.date <- simulation.data.annual.ma %>% 
+    group_by(scenario) %>% 
+    filter(temp.ma_c <= model.locations$start.spawn.temp_c) %>% 
+    arrange(date) %>% 
+    slice(1) %>% select(scenario, year.class, spawn.start.date = date)
   
+  ## Calculate the end date of spawning period
+  spawn.end.date <- simulation.data.annual.ma %>% 
+    group_by(scenario) %>% 
+    filter(temp.ma_c <= model.locations$end.spawn.temp_c) %>% 
+    arrange(date) %>% 
+    slice(1) %>% 
+    select(scenario, year.class, spawn.end.date = date) %>% 
+    ## subtract one day to correct for temp less than (spawning ends day of temp threshold)
+    mutate(spawn.end.date = as.Date(spawn.end.date)-1)
   
-})
+  ## Combine start and end dates; Filter to each day in spawning period
+  spawn.period.temp <- simulation.data.annual.ma %>% 
+    left_join(spawn.start.date) %>% 
+    left_join(spawn.end.date) %>%
+    group_by(scenario) %>% 
+    filter(date >= spawn.start.date, date <= spawn.end.date)
+  
+  ## Loop across all climate scenarios
+  do.call(rbind, lapply(unique(spawn.period.temp$scenario), function(j) {
+    spawn.period.temp.scenario <- spawn.period.temp %>% filter(scenario == j)
+    
+    ## Run model for each day in the spawning period
+    do.call(rbind, lapply(unique(spawn.period.temp.scenario$date), function(k) {
+      simulation.data.model <- simulation.data.annual %>% filter(scenario == j, date >= k)
+      
+      ## Take antilog from daily semilog output, accumulate across days
+      simulation.data.model.output <- simulation.data.model %>% 
+        mutate(perc.day = (10^(model.parameters$a + model.parameters$b * mean.temp.c + model.parameters$c * mean.temp.c^2))*100,
+               perc.cum = cumsum(perc.day),) %>% 
+        filter(perc.cum <= 100) %>%
+        mutate(ADD = cumsum(mean.temp.c))
+      
+      ## Extract hatch date
+      simulation.data.model.output.max <- simulation.data.model.output %>% 
+        slice(which.max(perc.cum)) %>% 
+        mutate(dpf = as.Date(date)-as.Date(k), 
+               hatch.yday = yday(date)) %>% 
+        select(scenario, year.class, hatch.date = date, hatch.yday, hatch.temp_c = mean.temp.c, dpf, ADD)
+    }))
+  }))
+})) %>% mutate(year.class = factor(year.class))
 
-
+ggplot(simulation.model.hatch, aes(x = year.class, y = hatch.yday)) +
+  geom_boxplot() +
+  coord_flip() +
+  theme_bw() +
+  facet_wrap(~scenario)
 
