39feab2e78efa9a24f21a119b5c4f00a08769834
Taylor Stewart <taylor.stewart@uvm.edu>
2021-07-09 17:45:10 -0400

    add regressions

--- scripts/lake-ontario-simulation-model.R
+++ scripts/lake-ontario-simulation-model.R
@@ -68,6 +68,14 @@ simulation.model.hatch.LO <- do.call(rbind, lapply(unique(simulation.data.filt$y
     ## subtract one day to correct for temp less than (spawning ends day of temp threshold)
     mutate(spawn.end.date = as.Date(spawn.end.date)-1)
   
+  if(i > 2006 & nrow(spawn.end.date) != 3) {
+    spawn.end.date <- spawn.start.date %>% left_join(spawn.end.date) %>% 
+      mutate(spawn.end.date = as.Date(ifelse(is.na(spawn.end.date) == TRUE, as.Date(spawn.start.date)+20, spawn.end.date), origin = "1970-01-01")) %>% 
+      select(-spawn.start.date)
+  } else {
+    spawn.end.date
+  }
+  
   ## Combine start and end dates; Filter to each day in spawning period
   spawn.period.temp <- simulation.data.annual.ma %>% 
     left_join(spawn.start.date) %>% 
@@ -148,6 +156,7 @@ simulation.model.hist.mean.LO <- simulation.model.hatch.LO %>%
             mean.hist.dpf = mean(dpf)) %>% 
   select(mean.hist.spawn.yday, mean.hist.hatch.yday, mean.hist.dpf)
 
+## calculate anomaly
 simulation.anomaly.LO <- simulation.model.hatch.LO %>%
   group_by(scenario) %>% 
   distinct(spawn.date, .keep_all = TRUE) %>% 
@@ -159,11 +168,72 @@ simulation.anomaly.LO <- simulation.model.hatch.LO %>%
          dpf.anomaly = dpf - mean.hist.dpf) %>% 
   group_by(scenario, year.class) %>% 
   summarize(mean.spawn.yday.anomaly = mean(spawn.yday.anomaly),
-            sd.spawn.yday.anomaly = sd(spawn.yday.anomaly),
             mean.hatch.yday.anomaly = mean(hatch.yday.anomaly),
-            sd.hatch.yday.anomaly = sd(hatch.yday.anomaly),
-            mean.dpf.anomaly = as.numeric(mean(dpf.anomaly)),
-            sd.dpf.anomaly = as.numeric(sd(dpf.anomaly)))
+            mean.dpf.anomaly = as.numeric(mean(dpf.anomaly)))
 
-rm("simulation.files", "simulation.data", "simulation.data.filt", "model.locations", "model.parameters", "simulation.model.hist.mean.LO")
+##
+trait.list <- c("mean.spawn.yday.anomaly", "mean.hatch.yday.anomaly", "mean.dpf.anomaly")
 
+simulation.anomaly.slope.LO <- do.call(rbind, lapply(trait.list, function(i) {
+  tmp.rcp <- simulation.anomaly.LO %>%  select(year.class, scenario, i) %>% 
+    filter(scenario != "Historical") %>% 
+    mutate(scenario = gsub(" ", "_", scenario)) %>% 
+    pivot_wider(names_from = scenario, values_from = i)
+  
+  ## Fit linear regressions
+  lm.2.6 <- lm(RCP_2.6 ~ year.class, data = tmp.rcp)
+  lm.6.0 <- lm(RCP_6.0 ~ year.class, data = tmp.rcp)
+  lm.8.5 <- lm(RCP_8.5 ~ year.class, data = tmp.rcp)
+  
+  ## extract slopes
+  coef.2.6 <- coef(lm.2.6)[2]
+  coef.6.0 <- coef(lm.6.0)[2]
+  coef.8.5 <- coef(lm.8.5)[2]
+  
+  ## extract r^2
+  R2.2.6 <- summary(lm.2.6)$r.squared
+  R2.6.0 <- summary(lm.6.0)$r.squared
+  R2.8.5 <- summary(lm.8.5)$r.squared
+  
+  
+  slopes <- data.frame(trait = rep(str_split(i, "[.]", simplify = TRUE)[1,2], 3),
+                       scenario = c("RCP 2.6", "RCP 6.0", "RCP 8.5"),
+                       slope = c(coef.2.6, coef.6.0, coef.8.5),
+                       R2 = c(R2.2.6, R2.6.0, R2.8.5))
+}))
+
+write.csv(simulation.anomaly.slope.LO, "data/anomaly-slopes/lake-ontario-slope.csv", row.names = FALSE)
+
+
+simulation.anomaly.comp.LO <- do.call(rbind, lapply(trait.list, function(i) {
+  tmp.rcp.factor <- simulation.anomaly.LO %>%  select(year.class, scenario, i) %>% 
+    filter(scenario != "Historical") %>% 
+    mutate(year.class = factor(year.class),
+           scenario = factor(scenario))
+  
+  ## Fit multiple regression for pairwise comparison
+  lm.factor <- lm(formula(paste(i, " ~ year.class + scenario")), data = tmp.rcp.factor)
+  p.value <- anova(lm.factor)[2,5]
+  
+  trait.comp <- data.frame(contrast = "overall",
+                           estimate = NA,
+                           SE = NA,
+                           df = NA,
+                           t.ratio = NA,
+                           p.value = p.value,
+                           trait = rep(str_split(i, "[.]", simplify = TRUE)[1,2]))
+  
+  if(p.value < 0.05) {
+    ## Calculate estimated marginal means and pairwise comparisons
+    trait.comp.all <- data.frame(pairs(emmeans(lm.factor, ~ scenario), simple = "scenario", adjust = "tukey")) %>% 
+      mutate(trait = str_split(i, "[.]", simplify = TRUE)[1,2]) %>% 
+      bind_rows(trait.comp)
+  } else {
+    trait.comp
+  }
+}))
+
+write.csv(simulation.anomaly.comp.LO, "data/anomaly-slopes/lake-ontario-multComp.csv", row.names = FALSE)
+
+
+rm("simulation.files", "simulation.data", "simulation.data.filt", "model.locations", "model.parameters", "simulation.model.hist.mean.LO", "trait.list")
--- scripts/lake-superior-apostle-islands-simulation-model.R
+++ scripts/lake-superior-apostle-islands-simulation-model.R
@@ -68,6 +68,14 @@ simulation.model.hatch.LS.APIS <- do.call(rbind, lapply(unique(simulation.data.f
     ## subtract one day to correct for temp less than (spawning ends day of temp threshold)
     mutate(spawn.end.date = as.Date(spawn.end.date)-1)
   
+  if(i > 2006 & nrow(spawn.end.date) != 3) {
+    spawn.end.date <- spawn.start.date %>% left_join(spawn.end.date) %>% 
+      mutate(spawn.end.date = as.Date(ifelse(is.na(spawn.end.date) == TRUE, as.Date(spawn.start.date)+20, spawn.end.date), origin = "1970-01-01")) %>% 
+      select(-spawn.start.date)
+  } else {
+    spawn.end.date
+  }
+  
   ## Combine start and end dates; Filter to each day in spawning period
   spawn.period.temp <- simulation.data.annual.ma %>% 
     left_join(spawn.start.date) %>% 
@@ -148,6 +156,7 @@ simulation.model.hist.mean.LS.APIS <- simulation.model.hatch.LS.APIS %>%
             mean.hist.dpf = mean(dpf)) %>% 
   select(mean.hist.spawn.yday, mean.hist.hatch.yday, mean.hist.dpf)
 
+## calculate anomaly
 simulation.anomaly.LS.APIS <- simulation.model.hatch.LS.APIS %>%
   group_by(scenario) %>% 
   distinct(spawn.date, .keep_all = TRUE) %>% 
@@ -159,10 +168,72 @@ simulation.anomaly.LS.APIS <- simulation.model.hatch.LS.APIS %>%
          dpf.anomaly = dpf - mean.hist.dpf) %>% 
   group_by(scenario, year.class) %>% 
   summarize(mean.spawn.yday.anomaly = mean(spawn.yday.anomaly),
-            sd.spawn.yday.anomaly = sd(spawn.yday.anomaly),
             mean.hatch.yday.anomaly = mean(hatch.yday.anomaly),
-            sd.hatch.yday.anomaly = sd(hatch.yday.anomaly),
-            mean.dpf.anomaly = as.numeric(mean(dpf.anomaly)),
-            sd.dpf.anomaly = as.numeric(sd(dpf.anomaly)))
+            mean.dpf.anomaly = as.numeric(mean(dpf.anomaly)))
   
+##
+trait.list <- c("mean.spawn.yday.anomaly", "mean.hatch.yday.anomaly", "mean.dpf.anomaly")
+
+simulation.anomaly.slope.LS.APIS <- do.call(rbind, lapply(trait.list, function(i) {
+  tmp.rcp <- simulation.anomaly.LS.APIS %>%  select(year.class, scenario, i) %>% 
+    filter(scenario != "Historical") %>% 
+    mutate(scenario = gsub(" ", "_", scenario)) %>% 
+    pivot_wider(names_from = scenario, values_from = i)
+  
+  ## Fit linear regressions
+  lm.2.6 <- lm(RCP_2.6 ~ year.class, data = tmp.rcp)
+  lm.6.0 <- lm(RCP_6.0 ~ year.class, data = tmp.rcp)
+  lm.8.5 <- lm(RCP_8.5 ~ year.class, data = tmp.rcp)
+  
+  ## extract slopes
+  coef.2.6 <- coef(lm.2.6)[2]
+  coef.6.0 <- coef(lm.6.0)[2]
+  coef.8.5 <- coef(lm.8.5)[2]
+  
+  ## extract r^2
+  R2.2.6 <- summary(lm.2.6)$r.squared
+  R2.6.0 <- summary(lm.6.0)$r.squared
+  R2.8.5 <- summary(lm.8.5)$r.squared
+  
+  ##
+  slopes <- data.frame(trait = rep(str_split(i, "[.]", simplify = TRUE)[1,2], 3),
+                       scenario = c("RCP 2.6", "RCP 6.0", "RCP 8.5"),
+                       slope = c(coef.2.6, coef.6.0, coef.8.5),
+                       R2 = c(R2.2.6, R2.6.0, R2.8.5))
+}))
+
+write.csv(simulation.anomaly.slope.LS.APIS, "data/anomaly-slopes/lake-superior-apostle-islands-slope.csv", row.names = FALSE)
+
+
+simulation.anomaly.comp.LS.APIS <- do.call(rbind, lapply(trait.list, function(i) {
+  tmp.rcp.factor <- simulation.anomaly.LS.APIS %>%  select(year.class, scenario, i) %>% 
+    filter(scenario != "Historical") %>% 
+    mutate(year.class = factor(year.class),
+           scenario = factor(scenario))
+  
+  ## Fit multiple regression for pairwise comparison
+  lm.factor <- lm(formula(paste(i, " ~ year.class + scenario")), data = tmp.rcp.factor)
+  p.value <- anova(lm.factor)[2,5]
+  
+  trait.comp <- data.frame(contrast = "overall",
+                           estimate = NA,
+                           SE = NA,
+                           df = NA,
+                           t.ratio = NA,
+                           p.value = p.value,
+                           trait = rep(str_split(i, "[.]", simplify = TRUE)[1,2]))
+  
+  if(p.value < 0.05) {
+    ## Calculate estimated marginal means and pairwise comparisons
+    trait.comp.all <- data.frame(pairs(emmeans(lm.factor, ~ scenario), simple = "scenario", adjust = "tukey")) %>% 
+      mutate(trait = str_split(i, "[.]", simplify = TRUE)[1,2]) %>% 
+      bind_rows(trait.comp)
+  } else {
+    trait.comp
+  }
+}))
+
+write.csv(simulation.anomaly.comp.LS.APIS, "data/anomaly-slopes/lake-superior-apostle-islands-multComp.csv", row.names = FALSE)
+
+## Clean environment
 rm("simulation.files", "simulation.data", "simulation.data.filt", "model.locations", "model.parameters", "simulation.model.hist.mean.LS.APIS")
